0b94836281966be997ba6cb0df4c71bd0f361ab0
rock <rock@rock.com>
2013-10-14 21:22:33 +0800

    aaa

--- Cmd.py
+++ Cmd.py
@@ -1,7 +1,15 @@
 import sublime, sublime_plugin
 import os
+import os.path
 class CmdCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		#os.system('cmd')
-		os.system("cmd /k cd /d D:/Program Files");
+		filleName = self.view.file_name()
+		drive = os.path.splitdrive(filleName)[0]
+		drive = drive[0,len(drive)-1]
+		dirNmae = os.path.dirname(filleName)
+		print dirNmae
+		#os.system("cmd /k cd /"+drive+" "+dirNmae);
+		self.view.insert(edit, 0, "cmd /k cd /"+drive+" "+dirNmae+"\n")
+		#os.system("cmd /k cd /d D:/Program Files");
 
