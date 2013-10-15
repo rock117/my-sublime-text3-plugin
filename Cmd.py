13b698d1ab692cff0b9e2459b3b2264286982628
Rock Huang <Rock.Huang@morningstar.com>
2013-10-15 09:16:41 +0800

    change CmdCommand

--- Cmd.py
+++ Cmd.py
@@ -1,7 +1,20 @@
 import sublime, sublime_plugin
 import os
+import os.path
 class CmdCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		#os.system('cmd')
-		os.system("cmd /k cd /d D:/Program Files");
-
+		fileName = self.view.file_name()
+		dirname = os.path.dirname(fileName)
+		os.chdir(dirname)
+		os.startfile('cmd')
+		# drive = os.path.splitdrive(fileName)[0].replace(':','')
+		# driveStr = "/"+drive+" "
+		# if(drive=='C' or drive=='c'):
+		# 	driveStr=''
+		# #os.system("cmd /k cd /d D:/Program Files");
+		# os.chdir('C:/Users/rhuang/AppData/Roaming/Sublime Text 3/Packages/User')
+		# f = open('runcmd.bat', 'w')
+		# f.write("cmd /k cd "+driveStr+dirname)
+		# f.close()
+		# os.startfile('runcmd.bat')
